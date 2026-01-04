f2fd250495ac8914ebef9c4cf494f11be73f200f
Delta <salmonzik01@gmail.com>
2026-01-04 16:55:25 +0200

    🚧

--- home/apps/vscode.nix
+++ home/apps/vscode.nix
@@ -9,6 +9,13 @@ let
       sha256 = "sha256-/mcaH3ms8yqfzTlaejH8xiT7wi19P7PdUeWS/6R+Ezg=";
     };
   };
+
+  sharedExtensions = with pkgs.vscode-extensions; [
+    github.copilot
+    github.copilot-chat
+  ] ++ [
+    solarized-theme
+  ];
 in
 
 {
@@ -19,21 +26,16 @@ in
   programs.vscode = {
     enable = true;
 
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      github.copilot
-      github.copilot-chat
-    ] ++ [
-      solarized-theme
-    ];
+    profiles.default.extensions = sharedExtensions;
 
     profiles.ts.extensions = with pkgs.vscode-extensions; [
       bradlc.vscode-tailwindcss
       esbenp.prettier-vscode
       styled-components.vscode-styled-components
-    ];
+    ] ++ sharedExtensions;
 
     profiles.nim.extensions = with  pkgs.vscode-extensions; [
       nimlang.nimlang
-    ];
+    ] ++ sharedExtensions;
   };
 }
