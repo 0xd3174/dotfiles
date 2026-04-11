d280ce3659900a3cbd474b99d69e7ac71c63026c
Delta <salmonzik01@gmail.com>
2026-04-11 11:39:44 +0200

    🚧

--- home/apps/vscode.nix
+++ home/apps/vscode.nix
@@ -19,6 +19,12 @@ let
 in
 
 {
+  imports = [
+    "${fetchTarball "https://github.com/msteen/nixos-vscode-server/tarball/master"}/modules/vscode-server/home.nix"
+  ];
+
+  services.vscode-server.enable = true;
+
   home.packages = with pkgs; [
     nim
     nimble
